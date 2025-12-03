c7d3136af01e9e95bed909b2bd9cea7c9d425d2c
Abhay Pardeshi <abhaypardeshi2210@gmail.com>
2025-12-03 23:48:45 +0530

    Intial Commit

---
+++ services/activityProducer/src/main/java/com/media/activityproducer/ActivityProducerApplication.java
@@ -0,0 +1,13 @@
+package com.media.activityproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivityProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActivityProducerApplication.class, args);
+	}
+
+}
---
+++ services/activityProducer/src/test/java/com/media/activity_producer/ActivityProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.media.activityproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActivityProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
